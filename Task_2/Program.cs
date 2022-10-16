100328af50ea16498d6782d43ebf1806b2b81fa4
Katy <bev08021982@gmail.com>
2022-10-16 18:54:49 +0400

    Задача 2 решила

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -4,3 +4,36 @@
 a = 5; b = 7 -> max = 7
 a = 2; b = 10 -> max = 10
 a = -9; b = -3 -> max = -3 */
+
+Console.WriteLine("Введите первое целое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Вы ввели {a} и {b}. Теперь сравним числа.");
+
+int max = 0;
+int min = 0;
+
+if (a > b)
+{
+    max = a;
+    Console.WriteLine($"max = {max}");
+}
+else
+{
+    max = b;
+    Console.WriteLine($"max = {max}");
+}
+
+if (a < b)
+{   min = a;
+    Console.WriteLine($"min = {min}"); }
+else
+{
+    max = b;
+    Console.WriteLine($"min = {min}");
+}

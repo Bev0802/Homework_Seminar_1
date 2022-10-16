9b3d58c2fbbb36a02402f3e7a40595622efc3927
Katy <bev08021982@gmail.com>
2022-10-16 19:02:51 +0400

    Задача 4 в поцессе

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -3,3 +3,19 @@
 2, 3, 7 -> 7
 44, 5, 78 -> 78
 22, 3, 9 -> 22 */
+Console.WriteLine("Введите первое целое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Вы ввели {a}, {b}, {c}. Теперь сравним числа.");
+
+int max = 0;
+

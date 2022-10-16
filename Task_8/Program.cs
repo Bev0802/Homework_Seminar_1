367bf1e287ac51dcf3d58824b2a94708f8f6324a
Katy <bev08021982@gmail.com>
2022-10-16 20:50:23 +0400

    Задача 8 в процессе

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -2,3 +2,17 @@
 а на выходе показывает все чётные числа от 1 до N.
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+Console.WriteLine("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1)
+    Console.WriteLine("Недопустимое число!");
+else
+{
+    int count = N;
+    while (count <= N)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+}
